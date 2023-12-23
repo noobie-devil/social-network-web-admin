1b10eb30ab66ca82b7912a2af0d111c9befd4e49
noobie-devil <school0123456@gmail.com>
2023-12-23 19:43:31 +0700

    Update web admin

---
+++ src/pages/ResourceManagementPage.vue
@@ -0,0 +1,121 @@
+<script setup>
+    import { storeToRefs } from 'pinia';
+    import { ref } from 'vue';
+
+    import useResourceStore from '@/stores/resourceStore.js';
+    import TableWithActions from '@/components/TableWithActions.vue';
+    import { translate } from '@/locales/translator.js';
+    import { getFormattedDate } from '@/utils/date';
+    import DatePickerMenu from '@/components/DatePickerMenu.vue';
+    import { basicRules } from '@/utils/validation.js';
+    import useFilterStore from '@/stores/filterStore';
+
+    const resourceStore = useResourceStore();
+    const { resources, totalItems, loading } = storeToRefs(resourceStore);
+
+    const name = ref(null);
+    const startYear = ref(null);
+    const addForm = ref(null);
+    const editForm = ref(null);
+    const formRules = ref(basicRules);
+
+    const filterStore = useFilterStore();
+    filterStore.setFilters([]);
+
+    const resourceHeaders = [
+        {
+            key: 'id',
+            title: translate('id'),
+            align: 'center',
+        },
+        {
+            key: 'name',
+            title: translate('name'),
+            align: 'center',
+        },
+        {
+            key: 'startYear',
+            title: translate('startYear'),
+            align: 'center',
+        },
+        {
+            key: 'actions',
+            title: translate('action'),
+            align: 'center',
+            sortable: false,
+        },
+    ];
+
+    const addItem = async () => {
+        if ((await addForm.value.validate()).valid) {
+            await resourceStore.addItem({
+                name: name.value,
+                startYear: +startYear.value,
+            });
+            name.value = '';
+            startYear.value = null;
+        }
+    };
+
+    const editItem = async (item) => {
+        if ((await editForm.value.validate()).valid) {
+            await resourceStore.editItem(item);
+        }
+    };
+</script>
+
+<template>
+    <TableWithActions
+        :headers="resourceHeaders"
+        :items="resources"
+        :loading="loading"
+        :itemsLength="totalItems"
+        @addItem="addItem"
+        @deleteItem="resourceStore.deleteItem"
+        @deleteItems="resourceStore.deleteItems"
+        @editItem="editItem"
+        @update-options="resourceStore.getItems"
+    >
+        <template #add>
+            <VContainer>
+                <VForm ref="addForm">
+                    <VRow class="justify-center align-center">
+                        <VCol cols="12">
+                            <VTextField
+                                class="my-3"
+                                :label="translate('name')"
+                                v-model.trim="name"
+                                :rules="formRules"
+                            ></VTextField>
+                        </VCol>
+                        <VCol cols="12" md="6">
+                            <VTextField v-model.trim="startYear" :label="translate('startYear')"></VTextField>
+                        </VCol>
+                    </VRow>
+                </VForm>
+            </VContainer>
+        </template>
+
+        <template #edit="{ item }">
+            <VContainer>
+                <VForm ref="editForm">
+                    <h2>ID: {{ item._id }}</h2>
+                    <VRow class="justify-center align-center">
+                        <VCol cols="12">
+                            <VTextField
+                                class="my-3"
+                                :label="translate('name')"
+                                hide-details
+                                v-model.trim="item.name"
+                                :rules="formRules"
+                            ></VTextField>
+                        </VCol>
+                        <VCol cols="12" md="6">
+                            <VTextField v-model.trim="item.startYear" :label="translate('startYear')"></VTextField>
+                        </VCol>
+                    </VRow>
+                </VForm>
+            </VContainer>
+        </template>
+    </TableWithActions>
+</template>
